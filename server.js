1f08be3735ead72a23a0eda0cbbc98baa76192f6
kratied <ksaxena@ed2100.com>
2024-07-25 00:59:58 +0530

    .

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 app.use(cors({
-    origin: "*"
+    origin: "https://soulmatch.co.in",
+    credentials: true
 }));
 // const MONGODBURI = 'mongodb://127.0.0.1:27017/soulMatch'
 const MONGODBURI =  'mongodb://13.200.211.15:27017/soulMatch';
