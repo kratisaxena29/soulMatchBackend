6fda35eac16a5ab2922ddcd96c3606b058ec0b83
kratied <ksaxena@ed2100.com>
2024-07-28 21:14:58 +0530

    .

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use(cors({
 // }))
 
 // const MONGODBURI = 'mongodb://127.0.0.1:27017/soulMatch'
-const MONGODBURI =  'mongodb://13.200.211.15:27017/soulMatch';
+// const MONGODBURI =  'mongodb://13.200.211.15:27017/soulMatch';
+const MONGODBURI = 'mongodb://admin:Pdi73vGW@13.200.211.15:27017/?authMechanism=DEFAULT&authSource=soulMatch'
 // const MONGODBURI = process.env.MONGO_URI
 mongoose.connect(MONGODBURI, {
     useUnifiedTopology: true,
