866fdb74209f544235e106dfe9576a7e01133acd
kratied <ksaxena@ed2100.com>
2024-07-28 22:55:18 +0530

    .

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ const allowedOrigins = ['https://soulmatch.co.in'];
 //     credentials: true
 // }));
 
-app.use(cors({
-    origin: 'https://soulmatch.co.in',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  }));
 // app.use(cors({
-//   origin: "*"  
-// }))
+//     origin: 'https://soulmatch.co.in',
+//     methods: ['GET', 'POST'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+//   }));
+app.use(cors({
+  origin: "*"  
+}))
 
 // const MONGODBURI = 'mongodb://127.0.0.1:27017/soulMatch'
-// const MONGODBURI =  'mongodb://13.200.211.15:27017/soulMatch';
-const MONGODBURI = 'mongodb://admin:Pdi73vGW@13.200.211.15:27017/?authMechanism=DEFAULT&authSource=soulMatch'
+const MONGODBURI =  'mongodb://13.200.211.15:27017/soulMatch';
+// const MONGODBURI = 'mongodb://admin:Pdi73vGW@13.200.211.15:27017/?authMechanism=DEFAULT&authSource=soulMatch'
 // const MONGODBURI = process.env.MONGO_URI
 mongoose.connect(MONGODBURI, {
     useUnifiedTopology: true,
